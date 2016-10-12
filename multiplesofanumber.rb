23459d013159599b059517adb80addccd0df9e7b
Corey Wofford <msucorey@gmail.com>
2016-10-12 11:05:53 -0400

    multiples of a number

---
+++ multiplesofanumber.rb
@@ -0,0 +1,31 @@
+=begin
+MULTIPLES OF A NUMBER
+CHALLENGE DESCRIPTION:
+Given numbers x and n, where n is a power of 2, print out the smallest multiple of n which is greater than or equal to x. Do not use division or modulo operator.
+INPUT SAMPLE:
+The first argument will be a path to a filename containing a comma separated list of two integers, one list per line. E.g.
+13,8
+17,16
+OUTPUT SAMPLE:
+Print to stdout, the smallest multiple of n which is greater than or equal to x, one per line. E.g.
+16
+32
+=end
+
+lines = File.readlines(ARGV[0])
+
+lines.each do |line|
+  numbers = line.split(",")
+  _x = numbers.first.to_i
+  _n = numbers.last.to_i
+  _multiple = _n
+  smallest_multiple_found = false
+  while !smallest_multiple_found
+    if _n >= _x
+      puts _n
+      smallest_multiple_found = true
+    else
+      _n += _multiple
+    end
+  end
+end
