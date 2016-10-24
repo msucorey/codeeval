e60adaf500adb19a1de206baba1a81c934c9f083
Corey Wofford <msucorey@gmail.com>
2016-10-24 17:46:01 -0400

    lowercasetext
    It gits (get it) no simpler

---
+++ lowercasetext.rb
@@ -0,0 +1,20 @@
+=begin
+LOWERCASE
+CHALLENGE DESCRIPTION:
+Given a string write a program to convert it into lowercase.
+INPUT SAMPLE:
+The first argument will be a path to a filename containing sentences, one per line. You can assume all characters are from the english language. E.g.
+HELLO CODEEVAL
+This is some text
+OUTPUT SAMPLE:
+Print to stdout, the lowercase version of the sentence, each on a new line. E.g.
+hello codeeval
+this is some text
+=end
+
+lines = File.readlines(ARGV[0])
+
+lines.each do |line|
+  words = line.split(" ").map { |word| word.downcase}
+  puts words.join(" ")
+end
